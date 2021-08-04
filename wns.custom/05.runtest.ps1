720b5988399648c8b02d9acbbf36e355bef4e4a2
Isuru Dilhan <inbox.isuru@gmail.com>
2021-08-04 14:35:23 +0530

    Update

--- wns.custom/05.runtest.ps1
+++ wns.custom/05.runtest.ps1
@@ -1,7 +1,11 @@
 # meant to be run inside docker container
 Invoke-WebRequest http://localhost -UseBasicParsing
 
-#.\seleniumtests\SeleniumDockerTest.exe http://localhost
+#copy settings.xml to .m2 folder
+cp \seleniumtests\settings.xml \Users\ContainerAdministrator\.m2
 
+Set-Location \seleniumtests\TestSelenium\; mvn clean test -Dsuite=regression -P=regression '-Dtestng.dtd.http=true'
 
-cd .\seleniumtests\TestSelenium\; mvn clean test -Dsuite=regression -P=regression '-Dtestng.dtd.http=true'
+Set-Location \seleniumtests\uniqueBuildTest\; mvn -Dtest=testUniqueBuild#testGoogleSearchAutomateTest27e test '-Dfile.encoding=UTF8'
+
+Set-Location \
